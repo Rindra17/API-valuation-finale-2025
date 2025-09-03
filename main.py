16327aa63a18ef0318d1a6aa3193bd9f9673d2a4
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2025-09-03 11:49:47 +0300

    feat: POST/ cars

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from fastapi import FastAPI
-from starlette.responses import Response
+from pydantic import BaseModel
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
@@ -7,3 +10,25 @@ app = FastAPI()
 def ping():
     return Response( content="pong", status_code=200, media_type="text/plain" )
 
+class CarCharacteristics(BaseModel):
+    max_speed: int
+    max_fuel_capacity: int
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: CarCharacteristics
+
+cars_list: List[Car] = []
+
+def serialize_cars():
+    car_serialized = []
+    for car in cars_list:
+        car_serialized.append(car.model_dump())
+    return car_serialized
+@app.post("/cars")
+def create_cars(cars: Car):
+    cars_list.append(cars)
+    serialized_car = serialize_cars()
+    return JSONResponse(content=serialized_car, status_code=201, media_type="application/json")
